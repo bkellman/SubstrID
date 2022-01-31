36abdc11c37904ee7032297cf93eceb756ed81d5
bkellman <benjamin.kellman@gmail.com>
2022-01-31 11:15:13 -0800

    correct inputs

--- code/1-R-psuedoAbnd.r
+++ code/1-R-psuedoAbnd.r
@@ -1,8 +1,31 @@
-#write psuedo-abundance
-r = read.csv('data-raw/glycan_sequences_glycoct.csv')
-r2 = matrix(1,nrow=nrow(r))
+library(dplyr)
+library(jsonlite)
+
+glycans = do.call(
+	cbind,
+	as.data.frame(
+		fromJSON('data-raw/GlyTouCan-GlycoCT-Human-Mouse.json')[[2]]$bindings))
+
+glycans %>% 
+	filter(duplicated(glytoucan.value,fromLast=T)|duplicated(glytoucan.value,fromLast=F)) %>%
+	select(glytoucan.value,glycoct.value,wurcs.value) %>% arrange(glytoucan.value) %>% View()
+
+
+# parse glycans
+df_glycoct = data.frame( Name= glycans$glytoucan.value,
+						'Glycan Structure'=glycans$glycoct.value) 
+
+df_wurcs = data.frame( Name= glycans$glytoucan.value,
+						'Glycan Structure'=glycans$wurcs.value) 
+
+write.csv(df_glycoct,file='data-raw/GlyTouCan-GlycoCT.csv')
+write.csv(df_wurcs,file='data-raw/GlyTouCan-WURCS.csv')
+
+# spoof abundance
+r2 = matrix(1,nrow=nrow(glycans))
 #r2 = matrix(runif(nrow(r)),nrow=nrow(r))
 r3 = t(r2)
-colnames(r3) = r$Name
+colnames(r3) = glycans$glytoucan.value
 rownames(r3) = c('A')
-write.csv(r3,file='data-raw/glycan_sequences_glycoct.abundance.csv',quote=F)
+write.csv(r3,file='data-raw/GlyTouCan-abundance.csv',quote=F)
+
--- code/2-sh-runGlycompare.sh
+++ code/2-sh-runGlycompare.sh
@@ -3,4 +3,8 @@
 # python glyCompare.py structure -a <ABUNDANCE TABLE> -v <VARIABLE ANNOTATION> 
 # -o <OUTPUT_DIRECTORY> -p <GLYCAN_DATA_TYPE> [-n <NORMALIZATION_MODE>, 
 # -m <SUBSTRUCTURE_ABUNDANCE_MULTIPLIER>, -c <NUMBER_OF_CORES>, -r <ROOT>, -u <CUSTOM_ROOT>, -d, -s, -b]
-python code/GlyCompareCT/glyCompareCT.py structure -a data-raw/glycan_sequences_glycoct.abundance.csv -v data-raw/glycan_sequences_glycoct.csv -o data-final/ -p glycoCT -c 20
+
+python code/GlyCompareCT/glyCompareCT.py structure \
+	-a data-raw/GlyTouCan-GlycoCT.csv \
+	-v data-raw/GlyTouCan-abundance.csv \
+	-o data-final/glytoucan_glycoct. -p glycoCT -c 20
